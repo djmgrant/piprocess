e50500be0699b8a92215701524309b4a2cb76068
djmgrant <djmgrant@gmail.com>
2021-12-01 14:10:29 +0000

    aded sudo to start/stop process

--- sysdsvc.py
+++ sysdsvc.py
@@ -6,7 +6,6 @@ Created on Wed Dec  1 12:31:41 2021
 @author: Dave Grant 
 """
 import subprocess
-import os 
 import sys
 
 def Systemctl(service, cmd):
@@ -14,15 +13,15 @@ def Systemctl(service, cmd):
     if cmd not in ('stop','start','status'):
         print('Command must be stop/start/status')
         exit(1)
-    else: 
-        print('exec cmd',service, cmd)
-        
+    else:    
         if cmd == 'status':
             stat = subprocess.call(["systemctl", "is-active", "--quiet", service])
             if(stat == 0):  # if 0 (active), print "Active"
                 print("Active")
         else:
-            command = 'service ' + service + ' ' + cmd 
+            stat = subprocess.call(["sudo systemctl", "--quiet", cmd, service])
+            if(stat == 0):  
+                print("Success")
     
 if __name__ == '__main__':
     monitor = Systemctl(sys.argv[1],sys.argv[2])
